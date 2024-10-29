5d9730d950300d9f7ffde2e962aca8e935909424
emmanuel medina <emmanuel.medina@doane.edu>
2024-10-29 17:07:13 -0500

    finish 1st tutorial

--- FirstNodeApp/app.js
+++ FirstNodeApp/app.js
@@ -44,11 +44,34 @@ fs.readdir('./', function(err, files) {
 
 /*
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function() {
-    console.log('Listener called');
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
 });
 
-emitter.emit('messageLogged');
+logger.log('message');
 */
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1, 2, 3]));
+        res.end();
+    }
+
+});
+
+
+
+server.listen(3000);
+console.log('listening on port 3000...')
--- FirstNodeApp/logger.js
+++ FirstNodeApp/logger.js
@@ -1,10 +1,19 @@
+/*
 console.log(__filename);
 console.log(__dirname);
+*/
+const EventEmitter = require('events');
 
 var url = "http://mylogger.io/log";
 
-function log(message) {
-    console.log(message);
+class Logger extends EventEmitter {
+    log(message) {
+         console.log(message);
+    
+        this.emit('messageLogged',{id: 1, url: ' http://'});
+    }
 }
 
-module.exports = log;
+
+
+module.exports = Logger;
