e33844d5d2233c7e0fb10315041fdf63de4bf4d7
emmanuel medina <emmanuel.medina@doane.edu>
2024-10-29 22:51:00 -0500

    finish 2nd tutorial

--- ExpressDemo/index.js
+++ ExpressDemo/index.js
@@ -17,7 +17,20 @@ app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
+app.get('/api/courses/:id', (req, res) => {
+    const course =courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        return res.status(404).send('Not found')
+    }
+
+    res.send(course);
+});
+
 app.post('/api/courses',(req, res) => {
+    if (!req.body.name || req.body.name.length < 3) {
+        return res.status(400).send('Minimum 3 characters');
+    }
+    
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -25,10 +38,31 @@ app.post('/api/courses',(req, res) => {
     courses.push(course);
     res.send(course);
 });
-app.get('/api/courses/:id', (req, res) => {
+
+app.put('/api/courses/:id',(req, res) => {
     const course =courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('Not found')
-        res.send(course);
+    if (!course) {
+        return res.status(404).send('Not found');
+    }
+
+    if (!req.body.name || req.body.name.length < 3) {
+        return res.status(400).send('Minimum 3 characters');
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id',(req, res) => {
+    const course =courses.find(c => c.id === parseInt(req.params.id));
+    if (!course){
+        return res.status(404).send('Not found');
+    } 
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
