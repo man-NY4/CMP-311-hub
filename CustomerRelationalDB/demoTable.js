7059705b63c38ec75008dced7ca6c5e8a36455df
emmanuel medina <emmanuel.medina@doane.edu>
2024-11-15 11:28:27 -0600

    make alter and normal table separate

--- CustomerRelationalDB/demoTable.js
+++ CustomerRelationalDB/demoTable.js
@@ -8,11 +8,11 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
-  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table altered");
-  });
+    console.log("Connected!");
+    var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
 });
