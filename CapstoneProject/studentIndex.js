0d0479e13d82e55d089ffa5dda9ab98a6b89fdf6
emmanuel medina <emmanuel.medina@doane.edu>
2024-11-20 11:44:45 -0600

    finish student part i think

--- CapstoneProject/studentIndex.js
+++ CapstoneProject/studentIndex.js
@@ -89,7 +89,7 @@ app.post('/api/capstone',(req, res) => {
 
 });
 
-app.put('/api/capstone',(req, res) => {
+app.put('/api/capstone/:id',(req, res) => {
     let con = mysql.createConnection({
         host: "localhost",
         user: "emmanuel_medina",
@@ -97,9 +97,45 @@ app.put('/api/capstone',(req, res) => {
         database: "mydb"
     });
 
-
+    con.connect(function(err) {
+        studentLast = req.body.lastName
+        studentFirst = req.body.firstName
+        studentEmail = req.body.email
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "UPDATE students SET lastName = ?, firstName = ?, email = ? WHERE id =" + parseInt(req.params.id);
+        con.query(sql, [studentLast, studentFirst, studentEmail], function (err, result) {
+          if (err) throw err;
+          else{
+            console.log("1 record updated");
+            if (!req.body.lastName || req.body.lastName.length < 3 || !req.body.firstName || req.body.firstName.length < 3) 
+                return res.status(400).send('Minimum 3 characters');
+                res.send(result)
+          }
+        });
+    });
 });
 
+
+app.delete('/api/capstone/:id',(req, res) => {
+    let con = mysql.createConnection({
+        host: "localhost",
+        user: "emmanuel_medina",
+        password: "Frog2570739707!",
+        database: "mydb"
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        var sql = "DELETE FROM students WHERE id =" + parseInt(req.params.id);
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Number of records deleted: " + result.affectedRows);
+          res.send(result)
+        });
+    });
+
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
